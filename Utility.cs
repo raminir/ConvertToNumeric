6be14c39071f0b6943efdd303542b784bcd36e06
Ramin Firouzabadi <40071865+raminir@users.noreply.github.com>
2022-09-18 13:39:17 +0430

    Create Utility.cs

---
+++ Utility.cs
@@ -0,0 +1,21 @@
+using System.Text;
+
+namespace Infrastructure
+{
+    public static class Utility
+    {
+        static Utility()
+        {
+        }
+
+        public static string? ConvertToNumeric(string? text)
+        {
+            var result = new StringBuilder(text!.Length);
+            foreach (char ch in text)
+            {
+                result.Append(char.IsDigit(ch) ? char.GetNumericValue(ch) : ch.ToString());
+            }
+            return result.ToString();
+        }
+    }
+}
